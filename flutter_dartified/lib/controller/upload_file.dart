f40a47c87e6d633e1dce0b8a942309269cb84a44
fz3hra <mujorefaatimah@gmail.com>
2023-10-17 20:04:48 +0400

    :sparkles: add functionality

--- flutter_dartified/lib/controller/upload_file.dart
+++ flutter_dartified/lib/controller/upload_file.dart
@@ -1,16 +1,36 @@
-import 'dart:convert';
+import 'dart:async';
 import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class UploadDocumentService {
-  static Future<String?> uploadFiles(BuildContext context, result) async {
-    print("result $result");
+  static StreamController<double> uploadProgressController =
+      StreamController<double>.broadcast();
+
+  static Future<String?> uploadFiles(
+    BuildContext context,
+    isUploading,
+    Function(dynamic) onUploading,
+  ) async {
     try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        allowMultiple: false,
+      );
       if (result != null) {
         File fileName = File(result.files.single.path!);
+        isUploading = true;
+        onUploading(isUploading);
+        // await Future.delayed(Duration(seconds: 3));
+        for (int i = 0; i <= 3; i++) {
+          uploadProgressController.sink.add(i / 3);
+          await Future.delayed(Duration(seconds: 1));
+        }
+        isUploading = false;
+        onUploading(isUploading);
         print("file $fileName");
-      } else {} // User canceled the picker
+      } else {
+        // User canceled the picker
+      }
     } catch (e, s) {
       rethrow;
     }
--- flutter_dartified/lib/screens/home_screen/home_screen.dart
+++ flutter_dartified/lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dartified/controller/upload_file.dart';
 import 'package:flutter_dartified/utils/constants/image_constants.dart';
@@ -8,8 +6,25 @@ import 'package:flutter_dartified/widgets/export_widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:lottie/lottie.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isUploading = false;
+  double progressValue = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    UploadDocumentService.uploadProgressController.stream.listen((progress) {
+      setState(() {
+        progressValue = progress;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,97 +32,126 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: const Color(0xFFCCEBFF),
       body: Container(
         width: double.infinity,
-        child: Column(
-          children: [
-            Image.asset(
-              ImageConstants.singleLogo,
-              color: Colors.black,
-            ),
-            const Gap(46),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Welcome James,",
-                style: const TextStyle()
-                    .spaceGrotesk
-                    .normal
-                    .colored(Colors.black)
-                    .sized(20.0),
-              ),
-            ),
-            const Gap(8),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Let's Get Started.",
-                style: const TextStyle()
-                    .spaceGrotesk
-                    .bold
-                    .colored(Colors.black)
-                    .sized(24.0),
-              ),
-            ),
-            const Gap(74),
-            Container(
-              width: 228,
-              height: 186,
-              decoration: BoxDecoration(
-                color: const Color(0xFFAFDFFF),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: FractionallySizedBox(
-                widthFactor: 0.7,
-                heightFactor: 0.7,
-                child: Lottie.asset(
-                  "assets/icons/upload_icon.json",
+        child: _isUploading == true
+            ? Center(
+                child: Container(
+                  width: 310,
+                  height: 222,
+                  padding: const EdgeInsets.all(32.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFF1D2327),
+                  ),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        ImageConstants.subLogoImage,
+                        width: 144,
+                      ),
+                      const Gap(16),
+                      Text(
+                        "A.I Processing PDF File",
+                        style: const TextStyle()
+                            .spaceGrotesk
+                            .bold
+                            .colored(Colors.white)
+                            .sized(24.0),
+                      ),
+                      const Gap(16),
+                      LinearProgressIndicator(
+                        value: progressValue,
+                        color: const Color(0xFF20577C),
+                      ),
+                    ],
+                  ),
                 ),
+              )
+            : Column(
+                children: [
+                  Image.asset(
+                    ImageConstants.singleLogo,
+                    color: Colors.black,
+                  ),
+                  const Gap(46),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Welcome James,",
+                      style: const TextStyle()
+                          .spaceGrotesk
+                          .normal
+                          .colored(Colors.black)
+                          .sized(20.0),
+                    ),
+                  ),
+                  const Gap(8),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Let's Get Started.",
+                      style: const TextStyle()
+                          .spaceGrotesk
+                          .bold
+                          .colored(Colors.black)
+                          .sized(24.0),
+                    ),
+                  ),
+                  const Gap(74),
+                  Container(
+                    width: 228,
+                    height: 186,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFAFDFFF),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.7,
+                      heightFactor: 0.7,
+                      child: Lottie.asset(
+                        "assets/icons/upload_icon.json",
+                      ),
+                    ),
+                  ),
+                  const Gap(40),
+                  Text(
+                    "Upload Your PDF File",
+                    style: const TextStyle()
+                        .spaceGrotesk
+                        .bold
+                        .colored(const Color(0xFF1D2327))
+                        .sized(32.0),
+                  ),
+                  const Gap(8),
+                  Text(
+                    "Upload your PDF file for our A.I to \nread and help you study better",
+                    style: const TextStyle()
+                        .spaceGrotesk
+                        .normal
+                        .colored(const Color(0xFF1D2327))
+                        .sized(18.0),
+                  ),
+                  const Gap(46),
+                  CustomButton(
+                    onPressed: () async {
+                      UploadDocumentService.uploadFiles(context, _isUploading,
+                          (onUploading) {
+                        setState(() {
+                          _isUploading = onUploading;
+                        });
+                      });
+                    },
+                    size: const Size(
+                      266,
+                      54.0,
+                    ),
+                    borderRadius: 10.0,
+                    buttonName: 'PROCEED',
+                    backgroundColor: const Color(0xFF1D2327),
+                    borderColor: Colors.transparent,
+                    textColor: Colors.white,
+                  ),
+                ],
               ),
-            ),
-            const Gap(40),
-            Text(
-              "Upload Your PDF File",
-              style: const TextStyle()
-                  .spaceGrotesk
-                  .bold
-                  .colored(const Color(0xFF1D2327))
-                  .sized(32.0),
-            ),
-            const Gap(8),
-            Text(
-              "Upload your PDF file for our A.I to \nread and help you study better",
-              style: const TextStyle()
-                  .spaceGrotesk
-                  .normal
-                  .colored(const Color(0xFF1D2327))
-                  .sized(18.0),
-            ),
-            const Gap(46),
-            CustomButton(
-              onPressed: () async {
-                FilePickerResult? result = await FilePicker.platform.pickFiles(
-                  allowMultiple: false,
-                );
-                if (result != null) {
-                  List<File> files =
-                      result.paths.map((path) => File(path!)).toList();
-                  print("files $files");
-                } else {
-                  // User canceled the picker
-                }
-                UploadDocumentService.uploadFiles(context, result);
-              },
-              size: const Size(
-                266,
-                54.0,
-              ),
-              borderRadius: 10.0,
-              buttonName: 'PROCEED',
-              backgroundColor: const Color(0xFF1D2327),
-              borderColor: Colors.transparent,
-              textColor: Colors.white,
-            ),
-          ],
-        ),
       ).addMarginizedContainer(),
     );
   }
